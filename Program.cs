63b51cc286efa8e9d840a2038bd130e9024f8991
stephen4018 <stephenohaeri4018@gmail.com>
2023-01-31 21:53:11 +0100

    refactored code and operation

--- Program.cs
+++ Program.cs
@@ -4,26 +4,28 @@ class Program
 {
     public static void Main()
     {
-        int result1 = 0;
-        int result2 = 0;
+        double result1 = 0;
+        double result2 = 0;
         string num1;
         string num2;
         string operate;
         string response;
-        //NUM 1 INPUT AND CHECK
         do
         {
+            //NUM 1 INPUT AND CHECK
+
+
             do
             {
                 Console.Write("please choose your first number: ");
                 num1 = Console.ReadLine();
 
 
-                if (!int.TryParse(num1, out result1))
+                if (!Double.TryParse(num1, out result1))
                 {
                     Console.WriteLine("Try Again And Please Enter A Valid Number");
                 }
-            } while (!int.TryParse(num1, out result1));
+            } while (!Double.TryParse(num1, out result1));
 
             //OPERATOR INPUT AND CHECK
             do
@@ -44,17 +46,13 @@ class Program
                     Console.Write("Please Choose Your Second Number: ");
                     num2 = Console.ReadLine();
 
-                    if (int.Parse(num2) == 0 && operate == "/")
+                     if (!Double.TryParse(num2, out result2))   // ***************TRY TO DEBUG THIS BY NOON******************
                     {
-                        Console.WriteLine("THIS WILL PRODUCE A SYNTAX ERROR, USE A CORRECT INPUT");
+                        Console.WriteLine("Try Again And Please Enter A Valid Number.");
                     }
-
-                        if (!int.TryParse(num2, out result2))   // ***************TRY TO DEBUG THIS BY NOON******************
-                    {
-                        Console.WriteLine("Try Again And Please Enter A Valid Number. you did it before, i am warning you now. just dey play");
-                    }
-                } while (!int.TryParse(num2, out result2));
-            } while (int.Parse(num2) == 0 && operate == "/");
+                      
+                } while (!Double.TryParse(num2, out result2));
+            } while (double.Parse(num2) == 0 && operate == "/");
 
             //OPERATION CONDITIONS********************************************************************
 
@@ -81,45 +79,49 @@ class Program
 
             Console.WriteLine("Done? YES OR NO");
             response = Console.ReadLine().ToUpper();
+
             if (response == "YES")
                 Console.WriteLine("THANK YOU FOR BANKING WITH US, SEE YOU SOME OTHER TIME");
+
         } while (response != "YES");
 
     }
 
+   
+
     // METHODS FOR MATHEMATICAL OPERATION
-    public static void Addition( int num1, int num2)
+    public static void Addition( double num1, double num2)
     {
 
-       int result = num1 + num2;
+       double result = num1 + num2;
         Console.WriteLine($"{num1} + {num2} = {result}"); 
     }
 
-    public static void Subtraction(int num1, int num2)
+    public static void Subtraction(double num1, double num2)
     {
 
-        int result = num1 - num2;
+        double result = num1 - num2;
         Console.WriteLine($"{num1} - {num2} = {result}");
     }
 
-    public static void Multiplication(int num1, int num2)
+    public static void Multiplication(double num1, double num2)
     {
-
-        int result = num1 * num2;
+        
+        double result = num1 * num2;
         Console.WriteLine($"{num1} * {num2} = {result}");
     }
 
-    public static void Remainder(int num1, int num2)
+    public static void Remainder(double num1, double num2)
     {
 
-        int result = num1 % num2;
+        double result = num1 % num2;
         Console.WriteLine($"{num1} % {num2} = {result}");
     }
 
-     public static void Division( int num1, int num2)
+     public static void Division( double num1, double num2)
     {
 
-       int result = num1 / num2;
+       double result = num1 / num2;
         Console.WriteLine($"{num1} / {num2} = {result}"); 
         
     }
